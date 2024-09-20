0957f60251ea96c5138221dd09692bb83b1cd1ce
bmgoncalves <brunomgoncalves@gmail.com>
2024-09-20 08:50:26 -0300

    Implementando formulario reativo

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-contato',
   standalone: true,
   imports: [
     ContainerComponent,
-    SeparadorComponent
-
+    SeparadorComponent,
+    ReactiveFormsModule
 ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
+
+  contatoForm!: FormGroup;
+  constructor() {
+    this.contatoForm = new FormGroup({
+      nome: new FormControl('dsdsad'),
+      telefone: new FormControl('fdgfdg'),
+      email: new FormControl('1312312'),
+      aniversario: new FormControl('01-01-2001'),
+      redes: new FormControl('12345'),
+      observacoes: new FormControl('5676876')
+    });
+
+  }
+
+
 
 }
