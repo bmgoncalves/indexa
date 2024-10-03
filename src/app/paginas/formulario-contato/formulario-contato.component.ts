0742b74ebc31737d9ed3f00209eb87be28b380d2
bmgoncalves <brunomgoncalves@gmail.com>
2024-10-03 09:57:04 -0300

    Separando responsabilidades - Formulario x Lista contatos

--- src/app/paginas/formulario-contato/formulario-contato.component.ts
+++ src/app/paginas/formulario-contato/formulario-contato.component.ts
@@ -1,34 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { SeparadorComponent } from '../../componentes/separador/separador.component';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-formulario-contato',
   standalone: true,
   imports: [
+    CommonModule,
     ContainerComponent,
     SeparadorComponent,
-    ReactiveFormsModule
-],
+    ReactiveFormsModule,
+    RouterLink
+  ],
   templateUrl: './formulario-contato.component.html',
   styleUrl: './formulario-contato.component.css'
 })
 export class FormularioContatoComponent {
 
   contatoForm!: FormGroup;
+
   constructor() {
     this.contatoForm = new FormGroup({
-      nome: new FormControl('dsdsad'),
-      telefone: new FormControl('fdgfdg'),
-      email: new FormControl('1312312'),
-      aniversario: new FormControl('01-01-2001'),
-      redes: new FormControl('12345'),
-      observacoes: new FormControl('5676876')
-    });
-
+      nome: new FormControl('', Validators.required),
+      telefone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      aniversario: new FormControl(''),
+      redes: new FormControl(''),
+      observacoes: new FormControl('')
+    })
   }
 
+  salvarContato() {
+    console.log(this.contatoForm.value);
+  }
 
-
+  cancelar() {
+    console.log('Submissão cancelada');
+  }
 }
---
+++ src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -0,0 +1,67 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CabecalhoComponent } from '../../componentes/cabecalho/cabecalho.component';
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { ContatoComponent } from '../../componentes/contato/contato.component';
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { FormularioContatoComponent } from '../formulario-contato/formulario-contato.component';
+import agenda from '../../agenda2.json'
+import { RouterLink } from '@angular/router';
+interface Contato {
+  id: number,
+  nome: string,
+  telefone: string
+}
+
+@Component({
+  selector: 'app-lista-contatos',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule,
+    FormularioContatoComponent,
+    RouterLink
+  ],
+  templateUrl: './lista-contatos.component.html',
+  styleUrl: './lista-contatos.component.css'
+})
+
+
+
+export class ListaContatosComponent {
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
+
+  filtroPorTexto: string = '';
+
+  filtrarContatosPorTexto(): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos;
+    }
+    return this.contatos.filter((contato) => {
+      return this.removerAcentos(contato.nome)
+        .toLowerCase()
+        .includes(this.removerAcentos(this.filtroPorTexto.toLowerCase()));
+    });
+  }
+
+  filtrarContatosPorLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatosPorTexto()
+               .filter((contato) => {
+                return this.removerAcentos(contato.nome)
+                           .toLowerCase()
+                           .startsWith(this.removerAcentos(letra)
+                                           .toLocaleLowerCase());
+    });
+  }
+
+  // Remove os acentos de uma string
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+}
