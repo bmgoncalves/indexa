1da64eaa46a356cec945fa8e8e552e2ee5d25f69
bmgoncalves <brunomgoncalves@gmail.com>
2024-10-23 16:00:56 -0300

    Utilizando API (MOC) - com Observable

--- src/app/services/contato.service.ts
+++ src/app/services/contato.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, OnInit } from '@angular/core';
 import { Contato } from '../componentes/contato/contato';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 
 
@@ -7,37 +9,18 @@ import { Contato } from '../componentes/contato/contato';
   providedIn: 'root'
 })
 export class ContatoService implements OnInit{
+  private readonly API = 'http://localhost:3000/contatos';
 
-  private contatos: Contato[] = [
-    {"id": 1, "nome": "Ana", "telefone": "29 278869420","email":"","aniversario":"","redes":"","observacoes":""},
-    {"id": 2, "nome": "Ágata", "telefone": "38 128451235","email":"","aniversario":"","redes":"","observacoes":""},
-    {"id": 3, "nome": "Bruno", "telefone": "95 695521583","email":"","aniversario":"","redes":"","observacoes":""},
-    {"id": 4, "nome": "Beatriz", "telefone": "25 854986459","email":"","aniversario":"","redes":"","observacoes":""},
-    {"id": 5, "nome": "Carlos", "telefone": "94 543197849","email":"","aniversario":"","redes":"","observacoes":""},
-    {"id": 7, "nome": "Daniel", "telefone": "56 613692441","email":"","aniversario":"","redes":"","observacoes":""},
-  ]
-
-  constructor() {
-    //Tentar obter os dados do localStorage
-
-  }
+  constructor(private http: HttpClient ) {}
   ngOnInit(): void {
-    const contatosLocalStorageString = localStorage.getItem('contatos');
-    const contatosLocalStorage =
-      contatosLocalStorageString ? JSON.parse(contatosLocalStorageString) : null;
 
-    this.contatos = contatosLocalStorage || null;
-
-    //Salvar os contatos no localStorage
-    localStorage.setItem('contatos', JSON.stringify(this.contatos));
   }
 
-  obterContatos() {
-    return this.contatos;
+  obterContatos(): Observable<Contato[]> {
+    return this.http.get<Contato[]>(this.API);
   }
 
   salvarContato(contato:Contato){
-    this.contatos.push(contato);
-    localStorage.setItem('contatos', JSON.stringify(this.contatos));
+
   }
 }
