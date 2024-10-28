4142acd118a7b73337ae7f1443e73994e99f38c2
bmgoncalves <brunomgoncalves@gmail.com>
2024-10-28 16:11:58 -0300

    Buscando contato por ID

--- src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { ContatoService } from './../../services/contato.service';
+import { Component, OnInit } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { Contato } from '../../componentes/contato/contato';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-perfil-contato',
   standalone: true,
-  imports: [ContainerComponent, ContainerComponent],
+  imports: [ContainerComponent, ContainerComponent, RouterLink],
   templateUrl: './perfil-contato.component.html',
   styleUrl: './perfil-contato.component.css',
 })
-export class PerfilContatoComponent {
+
+export class PerfilContatoComponent implements OnInit {
 
   contato: Contato = {
     nome: 'João',
@@ -20,4 +23,18 @@ export class PerfilContatoComponent {
     observacoes: '',
     id: 2,
   };
+
+  constructor(private activatedRoute: ActivatedRoute, private contatoService: ContatoService ){}
+
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    if(id){
+      this.contatoService.buscarPorId(parseInt(id)).subscribe((contatoParam) => {
+        this.contato = contatoParam;
+      });
+    }
+
+  }
+
 }
